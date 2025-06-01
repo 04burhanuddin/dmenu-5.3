4fc1422e6663638178c1e1ada8c3351d150e380e
04burhanuddin <dev.burhanuddin@gmail.com>
2025-06-02 00:39:52 +0700

    patch xresources-alt

--- config.def.h
+++ config.def.h
@@ -1,21 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;    /* -c option; centers dmenu on screen */
-static int min_width = 500; /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-  "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true",
+static int topbar           = 1;  /* -b option; if 0, dmenu appears at bottom */
+static char font[]          = "monospace:size=10"; /* -fn option overrides fonts[0]; default X11 font or font set */
+static int centered         = 1;   /* -c option; centers dmenu on screen */
+static int min_width        = 500;  /* minimum width when centered */
+static const char *fonts[]  = { "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true" };
+static char *prompt         = NULL; /* -p  option; prompt to the left of input field */
+static char normfgcolor[]   = "#bbbbbb";
+static char normbgcolor[]   = "#222222";
+static char selfgcolor[]    = "#eeeeee";
+static char selbgcolor[]    = "#005577";
+
+static char *colors[SchemeLast][2] = {
+  /*              fg            bg       */
+  [SchemeNorm] = { normfgcolor, normbgcolor },
+  [SchemeSel]  = { selfgcolor,  selbgcolor  },
+  [SchemeOut]  = { "#000000",   "#00ffff" },
 };
 
-static const char *prompt = NULL; /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-  /*              fg          bg       */
-  [SchemeNorm] = {"#bbbbbb", "#222222"},
-  [SchemeSel]  = {"#eeeeee", "#005577"},
-  [SchemeOut]  = {"#000000", "#00ffff"},
-};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
 
@@ -24,3 +27,15 @@ static unsigned int lines = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "font",        STRING, &font },
+	{ "normfgcolor", STRING, &normfgcolor },
+	{ "normbgcolor", STRING, &normbgcolor },
+	{ "selfgcolor",  STRING, &selfgcolor },
+	{ "selbgcolor",  STRING, &selbgcolor },
+	{ "prompt",      STRING, &prompt },
+};
